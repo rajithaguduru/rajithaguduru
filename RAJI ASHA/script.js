540ff91253994c6d25f06b04e587f1830381917f
rajithaguduru <124654274+rajithaguduru@users.noreply.github.com>
2024-06-19 10:59:59 +0530

    Add files via upload

---
+++ RAJI ASHA/script.js
@@ -0,0 +1,9 @@
+// Function to add the 'scrolled' class to the navbar on scroll
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
